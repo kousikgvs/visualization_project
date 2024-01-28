cdaa07185f59f7ac7f75131fec51b235448591f6
Kousikgvs <kousikgvs@gmail.com>
2024-01-28 19:04:36 +0530

    Committed

--- frontend/my-app/App.js
+++ frontend/my-app/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
 export default function App() {
+  const [data , setdata] = useState("")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>heyy my first app.</Text>
+      <input placeholder='Enter data' onChange={(event) => setdata(event.target.value)} />
+      <Text>{data}</Text>
+      {/* <Button></Button> */}
       <StatusBar style="auto" />
     </View>
   );
